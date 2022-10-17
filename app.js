aafc00fdd38af6f24293992d8ecc03f78f6cba16
Boris <boris.a.clark@icloud.com>
2022-10-17 19:30:08 -0400

    with sessions

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const morgan = require('morgan'); // this logs web traffic lots of different thi
 const path = require('path');
 const app = express();
 const PORT =process.env.PORT || 3000;
+const sessionsRouter=express.Router();
+
 app.use(morgan('tiny'));//tiny
 app.use(express.static(path.join(__dirname,'/public/'))); 
 //telling ejs where the templates will be
@@ -12,10 +14,28 @@ app.set('views','./src/views');
 app.set('view engine', 'ejs');
 
 
+//would normally be in sessionsRouter.js
+sessionsRouter.route('/')
+    .get((req,res)=> {
+        res.send('sessions',{session:[
+            {title:'Session 1', description:'this is session 1'},
+            {title:'Session 2', description:'this is session 2'},
+            {title:'Session 3', description:'this is session 3'},
+            {title:'Session 4', description:'this is session 4'}
+        ]});
+    })
+sessionsRouter.route('/1')
+    .get((req,res)=> {
+        res.send('hello single sessions')
+    });
+
+
 //req res sends to port
+app.use('/sessions', sessionsRouter);
 app.get('/',(req,res)=>{
-    res.render('index',{title:'Welcome to Globomantics',data:['a','b','c']});
+    res.render('index',{title:'Globomantics',data:['a','b','c']});
 });
+
 
 //tells to listen on port 3000
 app.listen(PORT,()=>{
